1e05ebbe14d8b162354a03e908569f8e7b7adc1c
irinkain <irina.inashvili.1@btu.edu.ge>
2020-07-11 14:38:32 +0400

    changed

--- final/src/pages/home/Home.jsx
+++ final/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useQuery } from 'react-apollo'
 import { Container, Button } from '@material-ui/core'
 import { GET_ALL_JOBS } from '../../graphql/query'
-import DataTable from '../../components/Datatable'
+import DataTable from '../../components/DataTable'
 import JobDrawer from './JobDrawer'
 import AddJobDrawer from './AddJobDrawer'
 
